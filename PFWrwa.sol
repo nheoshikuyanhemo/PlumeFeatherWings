1582c3fee5d80399fea30e456f8669999aa5babb
ZORG <32210699+nheoshikuyanhemo@users.noreply.github.com>
2025-04-27 07:31:36 +0700

    Update PFWrwa.sol

--- PFWrwa.sol
+++ PFWrwa.sol
@@ -1742,7 +1742,7 @@ contract WIZEToken is ERC20, Ownable {
             "H4G: The dividend tracker already has that address"
         );
 
-        WIZETokenDividendTracker newDividendTracker = WIZETokenDividendTracker(
+        PFWTokenDividendTracker newDividendTracker = WIZETokenDividendTracker(
             payable(newAddress)
         );
 
